1f73c405ec8f852fde4bb6c214fa9dddafd98f0a
unknown <IHA@LU52-15727-IHA-.VIA.DK>
2015-12-02 08:32:45 +0100

    Project files

--- Debug/makedep.mk
+++ Debug/makedep.mk
@@ -8,6 +8,22 @@ buffer\buffer.c
 
 dialog_handler\dialog_handler.c
 
+FreeRTOS\Source\croutine.c
+
+FreeRTOS\Source\event_groups.c
+
+FreeRTOS\Source\list.c
+
+FreeRTOS\Source\portable\GCC\ATMega256x\port.c
+
+FreeRTOS\Source\portable\MemMang\heap_1.c
+
+FreeRTOS\Source\queue.c
+
+FreeRTOS\Source\tasks.c
+
+FreeRTOS\Source\timers.c
+
 main.c
 
 serial\serial.c
